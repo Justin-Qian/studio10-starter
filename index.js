c54e17440f5701b2ca01edb312f7ecb05db252e9
Justin-Qian <yjqian19@gmail.com>
2025-04-24 00:43:02 -0400

    basic

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ createApp({
     return {
       myMessage: "",
       sending: false,
+      groupChatName: "",
+      creatingChat: false,
       channels: ["designftw"],
     };
   },
@@ -35,6 +37,29 @@ createApp({
       // Refocus the input field after sending the message
       await this.$nextTick();
       this.$refs.messageInput.focus();
+    },
+
+    async createGroupChat(session){
+      if (!groupChatName) return;
+
+      this.creatingChat = true;
+
+      await this.$graffiti.put({
+        value: {
+            activity: 'Create',
+            object: {
+              type: 'Group Chat',
+              name: this.groupChatName,
+              channel: crypto.randomUUID(), // This creates a random string
+            }
+        },
+        channels: this.channels,
+      },
+      session,
+      );
+
+      this.groupChatName = "";
+      this.creatingChat = false;
     },
   },
 })
